f0ec6490dc9544597e226d3e7a32fb3d88454088
root <root@localhost.localdomain>
2024-01-09 10:02:44 +0000

    Modified Graph

--- assign1/codes/impedance.py
+++ assign1/codes/impedance.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Impedance Function |H(jω)|
+# Function |H(jω)|
 def magnitude_H(omega, R, L, C):
     return np.sqrt(R**2 + (omega * L - 1/(omega * C))**2)
 
@@ -9,19 +9,19 @@ R_value = 7.4
 L_value = 3
 C_value = 0.000027
 
-omega_values = np.logspace(0, 3, 10000)
+omega_values = np.linspace(100, 200, 10000)
 
 magnitude_values = magnitude_H(omega_values, R_value, L_value, C_value)
 
-plt.plot(np.log10(omega_values), magnitude_values, label='|H(jω)|')
-plt.xlabel('log($\omega$)')
-plt.ylabel('|H(j$\omega$)|')
-plt.title('Magnitude Plot of H(j$\omega$)')
+plt.plot(omega_values, magnitude_values, label='|H(jω)|')
 
-plt.ylim(0, 30)
-plt.xlim(2, 2.1)
+plt.xlabel('Angular Frequency ($\omega$)')
+plt.ylabel('|H(j$\omega$)|')
+plt.title('Magnitude Plot of H(j$\omega$) for an RLC Circuit')
+
+plt.ylim(6, 10)
+plt.xlim(108, 115)
 
 plt.legend()
-
 plt.grid(True)
 plt.savefig('h_plot.png', dpi = 300, bbox_inches = 'tight')
