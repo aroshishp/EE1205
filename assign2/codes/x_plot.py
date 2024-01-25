55e6e3814816b4b9ad3eb12451c52fdaf65570d7
root <root@localhost.localdomain>
2024-01-25 10:05:11 +0000

    Revised Plot

--- assign2/codes/x_plot.py
+++ assign2/codes/x_plot.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#set p = q = 0.5
 q = 0.5
 
+#create discrete values of n
 n_values = np.arange(-6, 7, 1)
+#create corresponding vlaues of x(n)
 x_values = 2 * q * n_values * (n_values >= 0)
 
+#create stem plot
 plt.stem(n_values, x_values, linefmt='b-', markerfmt='bo', basefmt='r')
-plt.title('Plot of x(n) vs n')
 plt.xlabel('n')
 plt.ylabel('x(n)')
 plt.grid(True)
